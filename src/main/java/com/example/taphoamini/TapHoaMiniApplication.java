63814b1a3e4d1a30db4d4ef1ad236c7be1a5d909
345bc <tantuan87264@gmail.com>
2026-05-29 18:18:32 +0700

    feat: initialize Spring Boot project with Maven wrapper and dependencies

---
+++ src/main/java/com/example/taphoamini/TapHoaMiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.taphoamini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapHoaMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapHoaMiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taphoamini/TapHoaMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taphoamini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapHoaMiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
