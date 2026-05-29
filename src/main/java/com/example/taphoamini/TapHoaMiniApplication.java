2fb95df18b5acdfefc1669b28d0feb6789995acf
345bc <tantuan87264@gmail.com>
2026-05-29 18:18:35 +0700

    feat: initialize Spring Boot application with base project configuration and dependencies

--- src/main/java/com/example/taphoamini/TapHoaMiniApplication.java
+++ src/main/java/com/example/taphoamini/TapHoaMiniApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TapHoaMiniApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TapHoaMiniApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TapHoaMiniApplication.class, args);
+    }
 
 }
